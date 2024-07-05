2a6e27f3227c20ab227d5d8edcbf7e4bc172a329
Kevin Staunton-Lambert <kevstal@meta.com>
2024-07-05 16:51:50 +1000

    KJSL: JUCE updates

--- plugin/JuceLibraryCode/BinaryData.cpp
+++ plugin/JuceLibraryCode/BinaryData.cpp
@@ -4,6 +4,8 @@
 
 */
 
+#include <cstring>
+
 namespace BinaryData
 {
 
@@ -9256,6 +9258,7 @@ static const unsigned char temp_binary_data_26[] =
 const char* vital_word_ring_svg = (const char*) temp_binary_data_26;
 
 
+const char* getNamedResource (const char* resourceNameUTF8, int& numBytes);
 const char* getNamedResource (const char* resourceNameUTF8, int& numBytes)
 {
     unsigned int hash = 0;
@@ -9362,13 +9365,12 @@ const char* originalFilenames[] =
     "vital_word_ring.svg"
 };
 
+const char* getNamedResourceOriginalFilename (const char* resourceNameUTF8);
 const char* getNamedResourceOriginalFilename (const char* resourceNameUTF8)
 {
     for (unsigned int i = 0; i < (sizeof (namedResourceList) / sizeof (namedResourceList[0])); ++i)
-    {
-        if (namedResourceList[i] == resourceNameUTF8)
+        if (strcmp (namedResourceList[i], resourceNameUTF8) == 0)
             return originalFilenames[i];
-    }
 
     return nullptr;
 }
